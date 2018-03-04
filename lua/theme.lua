4ea59c24bef1d81ba19a52d77b287c4ee9df8438
Samuel Thayer <samonella7@gmail.com>
2018-03-04 11:08:43 -0700

    ug

---
+++ lua/theme.lua
@@ -0,0 +1,20 @@
+-- #textdomain wesnoth-utbs
+
+-- this is straight from UtBS, to make dazed status display
+
+local _ = wesnoth.textdomain "wesnoth-utbs"
+local old_unit_status = wesnoth.theme_items.unit_status
+
+function wesnoth.theme_items.unit_status()
+	local u = wesnoth.get_displayed_unit()
+	if not u then return {} end
+	local s = old_unit_status()
+
+	if u.status.double_dazed or u.status.single_dazed then
+		table.insert(s, { "element", { image = "misc/dazed-status-icon.png",
+			tooltip = _ "dazed: This unit is dazed. It suffers a -10% penalty to both its defense and chance to hit (except for magical attacks)."
+		} } )
+	end
+
+	return s
+end
